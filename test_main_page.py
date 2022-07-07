2a0cacc0a27cdc0ed4c7c27c9dc52261d50579c0
Elena <panaskoelena106@gmail.com>
2022-07-07 15:22:39 +0600

    First locators in test

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .locators import MainPageLocators
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
